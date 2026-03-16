0f0697e0e8abb4b2618c806f627b83f0219d71f9
Erilândio Santos Medeiros <hsymetry@gmail.com>
2026-03-16 20:59:07 -0300

    commit for vercel

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,4 +1,6 @@
+import 'package:detect_hans/views/home/home_page.dart';
 import 'package:detect_hans/views/questionnaire/questionnaire_page.dart';
+import 'package:detect_hans/views/story/story_page.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
@@ -7,10 +9,15 @@ class AppRouter {
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
+        return MaterialPageRoute(builder: (_) => const HomePage());
+      case '/story':
+        final story = settings.arguments as String?;
+        return MaterialPageRoute(builder: (_) => StoryPage(story: story));
+      case '/questionnaire':
         return MaterialPageRoute(builder: (_) => const QuestionnairePage());
       default:
         return MaterialPageRoute(
-          builder: (_) => const QuestionnairePage(),
+          builder: (_) => const Scaffold(body: Center(child: Text('Page not found'))),
         );
     }
   }
